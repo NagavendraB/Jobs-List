d0ebc8717a6c939de990b9d882ef58055eac1b7b
NagavendraB <nagu502@gmail.com>
2019-02-01 23:49:56 +0100

    Corrected the mistakely commited file

--- src/components/jobs-details.js
+++ src/components/jobs-details.js
@@ -1,42 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {Link} from 'react-router-dom';
+
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import ButtonBase from '@material-ui/core/ButtonBase';
+import {withStyles} from '@material-ui/core/styles';
 
 const styles = theme => ({
-  root: {
+  jobDetails: {
     flexGrow: 1,
-    textAlign: 'left'
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
+    textAlign: 'left',
     margin: 'auto',
     maxWidth: 500,
   },
-  img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+  }
 });
 
-const JobDetails = ({ children, selectedJob, classes }) => {
+const JobDetails = ({selectedJob, classes}) => {
   return (
-    <div className={classes.root}>
+    <div className={classes.jobDetails}>
+      <Link to='/'><Typography gutterBottom>Back to List of Jobs</Typography></Link>
       <Paper className={classes.paper}>
         <Grid container spacing={16}>
-          <Grid item xs container direction="column" spacing={16}>
+          <Grid item xs container direction='column' spacing={16}>
             <Grid item xs>
-              <Typography gutterBottom variant="subtitle1">
-                Job Details
-              </Typography>
-              <Typography gutterBottom>Job Title: {selectedJob.title}</Typography>
-              <Typography gutterBottom>Employement Type: {selectedJob.employment_type}</Typography>
-              <Typography gutterBottom>Discription: {selectedJob.description}</Typography>
+              <Typography gutterBottom variant='subtitle1'>Job Details</Typography>
+              <Typography gutterBottom>Job Title:- {selectedJob.title}</Typography>
+              <Typography gutterBottom>Employement Type:- {selectedJob.employment_type}</Typography>
+              <Typography gutterBottom>Discription:- {selectedJob.description}</Typography>
             </Grid>
           </Grid>
         </Grid>
@@ -46,3 +40,8 @@ const JobDetails = ({ children, selectedJob, classes }) => {
 };
 
 export default withStyles(styles)(JobDetails);
+
+JobDetails.propTypes = {
+  selectedJob: PropTypes.object,
+  classes: PropTypes.object
+};
